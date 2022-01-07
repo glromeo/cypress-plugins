02ea7dbdb9c0e4681a7c2a6cc1ffe99cd8e8ff3a
Gianluca Romeo <glromeo@gmail.com>
2022-01-07 17:01:25 +0000

    misc fixes

--- examples/native-coverage/cypress/integration/2-advanced-examples/location.spec.js
+++ examples/native-coverage/cypress/integration/2-advanced-examples/location.spec.js
@@ -14,12 +14,12 @@ context('Location', () => {
     // https://on.cypress.io/location
     cy.location().should((location) => {
       expect(location.hash).to.be.empty
-      expect(location.href).to.eq('http://localhost:3000/commands/location')
-      expect(location.host).to.eq('localhost:3000')
+      expect(location.href).to.eq('http://localhost:3300/commands/location')
+      expect(location.host).to.eq('localhost:3300')
       expect(location.hostname).to.eq('localhost')
-      expect(location.origin).to.eq('http://localhost:3000')
+      expect(location.origin).to.eq('http://localhost:3300')
       expect(location.pathname).to.eq('/commands/location')
-      expect(location.port).to.eq('3000')
+      expect(location.port).to.eq('3300')
       expect(location.protocol).to.eq('http:')
       expect(location.search).to.be.empty
     })
@@ -27,6 +27,6 @@ context('Location', () => {
 
   it('cy.url() - get the current URL', () => {
     // https://on.cypress.io/url
-    cy.url().should('eq', 'http://localhost:3000/commands/location')
+    cy.url().should('eq', 'http://localhost:3300/commands/location')
   })
 })
--- examples/native-coverage/cypress/integration/2-advanced-examples/navigation.spec.js
+++ examples/native-coverage/cypress/integration/2-advanced-examples/navigation.spec.js
@@ -2,7 +2,7 @@
 
 context('Navigation', () => {
   beforeEach(() => {
-    cy.visit('http://localhost:3000')
+    cy.visit('http://localhost:3300')
     cy.get('.navbar-nav').contains('Commands').click()
     cy.get('.dropdown-menu').contains('Navigation').click()
   })
